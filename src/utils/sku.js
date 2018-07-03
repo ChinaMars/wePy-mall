6bc2c77766546d1c25ca9c380bd6af50299b6d99
Mars <mars.huang@d1m.cn>
2018-07-03 17:52:24 +0800

    商品详情页面规格选择

--- src/utils/sku.js
+++ src/utils/sku.js
@@ -6,7 +6,7 @@ export default class Sku {
    */
   constructor(goodsAttrs) {
     this.goodsAttrs = goodsAttrs // 商品规格和不同规格的组合商品属性
-    this.defaultImage = '' // 默认商品图片
+    this.productImage = this.goodsAttrs.defaultImage // 商品图片
     this.attrList = this.goodsAttrs.attrList // 商品规格
     this.skuBeanList = this.goodsAttrs.skuBeanList // 规格组合商品
     this.enabledIds = [] // 选择规格对应的sku id
@@ -52,19 +52,19 @@ export default class Sku {
   checkSkuBean() {
     let skuBeanList = this.skuBeanList
     this.enabledIds = []
-    /* 循环得到组合商品attributeId */
+    // 循环得到组合商品attributeId
     for (let i in skuBeanList) {
       let attributes = (skuBeanList[i]).attributes
       for (let j in attributes) {
         let skuAttrBeanId = attributes[j].attributeId
         let skuAttrId = this.skuAttrId(this.attrList)
-        /* 检测组合商品中使用了哪些sku */
+        // 检测组合商品中使用了哪些sku
         if (include(skuAttrId, skuAttrBeanId)) {
           this.enabledIds.push(attributes[j].attributeValId)
         }
       }
     }
-    /* 合并数组中相同的值 */
+    // 合并数组中相同的值
     return uniq(this.enabledIds)
   }
 
@@ -96,13 +96,16 @@ export default class Sku {
       this.checkSelectedSku(skuBeanList, attrsOtherSelect, attrListBig)
     }
 
-    /* 合并数组中相同的值 */
+    // 合并数组中相同的值
     let enabledIds = uniq(this.enabledIds)
     this.checkEnable(this.attrList, enabledIds)
+
+    // 获取规格全部选择后的组合商品信息
+    this.groupedProSku(attrList)
   }
 
   /**
-   * 根据选中的sku获取对应的sku ID
+   * 根据选中的sku获取对应能选择的sku ID
    */
   checkSelectedSku(skuBeanList, attrsOtherSelect, attrListBig) {
     for (let i in skuBeanList) {
@@ -137,8 +140,6 @@ export default class Sku {
     this.attrsOtherSelectArray = []
     for (let i in this.attrList) {
       let attrListSmall = this.attrList[i]
-      console.log(attrListBig.id)
-      console.log(attrListSmall.id)
       if (attrListBig.id !== attrListSmall.id) {
         for (let i in attrListSmall.attr) {
           let attrListSmallAttr = attrListSmall.attr[i]
@@ -152,12 +153,50 @@ export default class Sku {
   }
 
   /**
+   * 获取选中后的组合商品属性
+   */
+  groupedProSku (attrList) {
+    let groupedProArray = []
+    for (let i in attrList) {
+      let attrListArray = attrList[i].attr
+      this.isReady = false
+      for (let i in attrListArray) {
+        if (attrListArray[i].select) {
+          this.isReady = true
+          groupedProArray.push(attrListArray[i].attributeValue)
+        }
+      }
+    }
+    if (this.isReady) {
+      this.getGroupedInfo(groupedProArray)
+    }
+  }
+
+  /**
+   * 通过sku组合确定组合商品信息
+    */
+  getGroupedInfo (groupedProArray) {
+    let groupedProText = ''
+    let skuBeanList = this.skuBeanList
+    for (let i in groupedProArray) {
+      groupedProText += groupedProArray[i] + '_'
+    }
+    for (let i in skuBeanList) {
+      let groupedProName = skuBeanList[i].name
+      if (groupedProText === groupedProName) {
+        console.log(skuBeanList[i].pic)
+        this.productImage = skuBeanList[i].pic
+      }
+    }
+  }
+
+  /**
    * 导出数据
    */
   export() {
     return {
-      defaultImage: this.defaultImage,
-      attrList: this.attrList
+      attrList: this.attrList,
+      productImage: this.productImage
     }
   }
 }
