60f015f6580a1748dc9f3ef1e6730ee59f901e5e
Mars <mars.huang@d1m.cn>
2018-07-16 13:44:24 +0800

    填写地址页面

--- src/mixins/bindTap.js
+++ src/mixins/bindTap.js
@@ -4,11 +4,19 @@ export default class bindTap extends wepy.mixin {
   methods = {
     /* 跳转指定商品详情页 */
     routeToGoods(goodsId) {
-      this.$parent.$navigate(`/pages/product/view?goodsId=${goodsId}`)
+      this.$root.$navigate(`/pages/product/view?goodsId=${goodsId}`)
     },
     /* 打开购物面板 */
     openBuyPanel(goodsAttrs) {
       Event.emit('goodsPanelOpen', goodsAttrs)
+    },
+    /* 跳转到地址列表页 */
+    routeToShippAddr() {
+      this.$root.$navigate(`/pages/address/list`)
+    },
+    /* 跳转到地址填写页 */
+    routeToAddAddress() {
+      this.$root.$navigate(`/pages/address/add`)
     }
   }
 }
